c6e607a17f75cbcd9573e51b354cfad905b3bf91
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-04-22 01:37:37 -0300

    back-end com front-end,style,create,post,del_users

--- devclub_cadastro_usuarios/src/pages/home/index.jsx
+++ devclub_cadastro_usuarios/src/pages/home/index.jsx
@@ -1,35 +1,104 @@
+import { useEffect ,useState,useRef} from 'react'
+
 import './style.css'
 import Trash from '../../assets/trash.svg'
+import api from '../../services/api'
+
+//React Hooks
+//React Hook -useRef
 
 function Home() {
+
+  const [users,set_users]=useState([])
+
+  const input_name=useRef()
+  const input_age=useRef()
+  const input_email=useRef()
+
   
+
+
+  async function get_users() {
+  
+    const users_from_api=await api.get('/usuarios')
+
+    set_users(users_from_api.data)
+  }
+
+
+  async function create_users() {
+  
+  await api.post('/usuarios',{
+    name:input_name.current.value,
+    age:input_age.current.value,
+    email:input_email.current.value
+  })
+
+  get_users()
+
+  
+
+    
+  }
+
+  async function del_users(id) {
+  
+    await api.delete(`/usuarios/${id}`)
+
+    get_users()
+  }
+  
+
+  useEffect(() => {
+
+    get_users()
+
+  }, [])
+  // const users = [
+  //   {
+  //     id: 1,
+  //     nome: 'Lucas',
+  //     idade: 20,
+  //     email: 'lucas@email.com'
+  //   },
+  //   {
+  //     id: 2,
+  //     nome: 'Aline',
+  //     idade: 25,
+  //     email: 'aline@email.com'
+  //   }
+
+  // ]
+
 
   return (
 
-      <div className='container'>
-        <form>
-          <h1>Cadstro de Usuarios</h1>
-          <input type="text" name='nome' />
-          <input type="number" name='idade' />
-          <input type="email" name='email' />
-          <button type='button'>Cadastrar</button>
-        </form>
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuarios</h1>
+        <input placeholder="Nome" type="text" name='nome' ref={input_name}/>
+        <input placeholder="Idade" type="number" name='idade' ref={input_age} />
+        <input placeholder="Email" type="email" name='email' ref={input_email}/>
+        <button type='button' onClick={create_users}>Cadastrar</button>
+      </form>
 
-        <div>
+      {users.map((user) => (
+        <div key={user.id} className='card'>
           <div>
-            <p>Nome:</p>
-            <p>Idade:</p>
-            <p>Email</p>
+            <p>Nome:  <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
           </div>
-          <button>
-            <img src="{Trash}" alt="" />
+          <button onClick={() =>del_users(user.id)}>
+            <img src={Trash} />
           </button>
         </div>
-      </div>
+      ))}
+    </div>
 
-     
-      
-    
+
+
+
   )
 }
 
